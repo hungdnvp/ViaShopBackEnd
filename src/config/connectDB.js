a8c7c023fbb0ad4e73cac08211935c096d6b400e
QuangHung <doquanghung25102001@gmail.com>
2023-08-18 16:24:23 +0700

    new machine

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,19 +1,23 @@
-const { Sequelize } = require('sequelize');
+require("dotenv").config();
 
+const { Sequelize } = require("sequelize");
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('viashop', 'root','', {
-  host: '127.0.0.1',
-  dialect: 'mysql',
-  logging: false
+const user_mysql = process.env.USER_MYSQL;
+const pass_mysql = process.env.PASS_MYSQL;
+
+const sequelize = new Sequelize("viashop", user_mysql, pass_mysql, {
+  host: "127.0.0.1",
+  dialect: "mysql",
+  logging: false,
 });
 
-let connectDB =async () =>{
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-}
+let connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 
 module.exports = connectDB;
