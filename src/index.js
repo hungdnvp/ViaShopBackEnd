a523be95d0fa8dfa48e53fbca59c84bad293ce0c
hungdnvp <doquanghung25102001@gmail.com>
2023-11-28 11:11:33 +0700

    disible https

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import cookieParser from "cookie-parser";
 require("dotenv").config();
 
 const fs = require("fs");
-const https = require("https");
+// const https = require("https");
 const port = process.env.PORT || 9090;
 
 let app = express();
 //config app
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(
   cors({
     origin: process.env.URL_FONT_END,
@@ -34,20 +34,20 @@ app.use(cookieParser());
 viewEngine(app);
 initWebRoutes(app);
 connectDB();
-https
-  .createServer(
-    {
-      key: fs.readFileSync("key.pem"),
-      cert: fs.readFileSync("cert.pem"),
-    },
-    app
-  )
-  .listen(port, () => {
-    //callback
-    console.log("backend is running on the port: " + port);
-  });
+// https
+//   .createServer(
+//     {
+//       key: fs.readFileSync("key.pem"),
+//       cert: fs.readFileSync("cert.pem"),
+//     },
+//     app
+//   )
+//   .listen(port, () => {
+//     //callback
+//     console.log("backend is running on the port: " + port);
+//   });
 
-// app.listen(port, () => {
-//   //callback
-//   console.log("backend is running on the port: " + port);
-// });
+app.listen(port, () => {
+  //callback
+  console.log("backend is running on the port: " + port);
+});
